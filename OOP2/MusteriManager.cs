0bb902f8b8efb8cbc3bffb160b7801411ed4acc4
enismkrc <58322533+enismkrc@users.noreply.github.com>
2021-01-23 23:51:44 +0300

    23 Ocak ders sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //Business classes
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+
+
+
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //enis makaracı
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "enis";
+            musteri1.Soyadi = "makaracı";
+            musteri1.TcNo = "123123123";
+
+            //kodlama.io
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "987654";
+            musteri2.SirketAdi = "kodlama.io";
+            musteri2.VergiNo = "1234567890";
+
+            //gerçek müsteri- tüzel müşteri
+            //SO'L'ID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+        }
+    }
+}
